452c48c17e0b8d37badb40eef1d4525a94e35ca5
zicla <ziclax@163.com>
2018-01-14 02:24:44 +0800

    Add mail validate util.

---
+++ rest/util_validation.go
@@ -0,0 +1,10 @@
+package rest
+
+import (
+	"regexp"
+)
+
+func ValidateEmail(email string) bool {
+	emailRegexp := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return emailRegexp.MatchString(email)
+}
