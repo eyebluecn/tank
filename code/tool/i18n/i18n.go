2b687331b55307f98825a4b25c5ebdb753d6b61f
zicla <ziclax@163.com>
2019-05-04 03:52:25 +0800

    Add the placeholder of i18n.

---
+++ code/tool/i18n/i18n.go
@@ -0,0 +1,16 @@
+package i18n
+
+const (
+	//英文
+	ENGLISH = "ENGLISH"
+	//中文
+	CHINESE = "CHINESE"
+)
+
+func Message(lang string, key string) {
+	if lang == ENGLISH {
+
+	} else if lang == CHINESE {
+
+	}
+}
