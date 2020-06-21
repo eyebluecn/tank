c711423ef905dcfe6630b8f3fb74dd7367af0214
lishuang <ziclax@163.com>
2020-06-21 18:22:00 +0800

    Add tank30 schema.

--- build/doc/sql/schema.sql
+++ build/doc/sql/schema.sql
@@ -1,85 +1,81 @@
-CREATE TABLE `tank30_user` (
+CREATE TABLE `tank30_bridge` (
   `uuid` char(36) NOT NULL DEFAULT '',
   `sort` bigint(20) NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `role` varchar(45) DEFAULT NULL,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `avatar_url` varchar(255) DEFAULT NULL,
-  `last_ip` varchar(128) DEFAULT NULL,
-  `last_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `size_limit` bigint(20) NOT NULL DEFAULT '-1',
-  `total_size_limit` bigint(20) NOT NULL DEFAULT '-1',
-  `total_size` bigint(20) NOT NULL DEFAULT '0',
-  `status` varchar(45) DEFAULT NULL,
+  `share_uuid` char(36) DEFAULT NULL,
+  `matter_uuid` char(36) DEFAULT NULL,
   PRIMARY KEY (`uuid`),
-  UNIQUE KEY `username` (`username`),
   UNIQUE KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tank30_upload_token` (
+CREATE TABLE `tank30_dashboard` (
+  `uuid` char(36) NOT NULL DEFAULT '',
+  `sort` bigint(20) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
+  `invoke_num` bigint(20) NOT NULL,
+  `total_invoke_num` bigint(20) NOT NULL DEFAULT '0',
+  `uv` bigint(20) NOT NULL DEFAULT '0',
+  `total_uv` bigint(20) NOT NULL DEFAULT '0',
+  `matter_num` bigint(20) NOT NULL DEFAULT '0',
+  `total_matter_num` bigint(20) NOT NULL DEFAULT '0',
+  `file_size` bigint(20) NOT NULL DEFAULT '0',
+  `total_file_size` bigint(20) NOT NULL DEFAULT '0',
+  `avg_cost` bigint(20) NOT NULL DEFAULT '0',
+  `dt` varchar(45) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `idx_dt` (`dt`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tank30_download_token` (
   `uuid` char(36) NOT NULL DEFAULT '',
   `sort` bigint(20) NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
   `user_uuid` char(36) NOT NULL,
-  `folder_uuid` char(36) NOT NULL,
   `matter_uuid` char(36) NOT NULL,
   `expire_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `filename` varchar(255) NOT NULL,
-  `privacy` tinyint(1) NOT NULL DEFAULT '0',
+  `ip` varchar(128) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `idx_mu` (`matter_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tank30_footprint` (
+  `uuid` char(36) NOT NULL DEFAULT '',
+  `sort` bigint(20) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
+  `user_uuid` char(36) DEFAULT NULL,
+  `ip` varchar(128) NOT NULL,
+  `host` varchar(45) NOT NULL,
+  `uri` varchar(255) NOT NULL,
+  `params` text,
+  `cost` bigint(20) NOT NULL DEFAULT '0',
+  `success` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tank30_image_cache` (
+  `uuid` char(36) NOT NULL DEFAULT '',
+  `sort` bigint(20) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
+  `name` varchar(255) NOT NULL,
+  `user_uuid` char(36) DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `matter_uuid` char(36) DEFAULT NULL,
+  `matter_name` varchar(255) NOT NULL,
+  `mode` varchar(512) DEFAULT NULL,
+  `md5` varchar(45) DEFAULT NULL,
   `size` bigint(20) NOT NULL DEFAULT '0',
-  `ip` varchar(128) NOT NULL,
+  `path` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `tank30_share` (
-  `uuid` char(36) NOT NULL DEFAULT '',
-  `sort` bigint(20) NOT NULL,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `name` varchar(255) DEFAULT NULL,
-  `share_type` varchar(45) DEFAULT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `user_uuid` char(36) DEFAULT NULL,
-  `download_times` bigint(20) NOT NULL DEFAULT '0',
-  `code` varchar(45) NOT NULL,
-  `expire_infinity` tinyint(1) NOT NULL DEFAULT '0',
-  `expire_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `tank30_session` (
-  `uuid` char(36) NOT NULL DEFAULT '',
-  `sort` bigint(20) NOT NULL,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `user_uuid` char(36) DEFAULT NULL,
-  `ip` varchar(128) NOT NULL,
-  `expire_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `tank30_preference` (
-  `uuid` char(36) NOT NULL DEFAULT '',
-  `sort` bigint(20) NOT NULL,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `name` varchar(45) DEFAULT NULL,
-  `logo_url` varchar(255) DEFAULT NULL,
-  `favicon_url` varchar(255) DEFAULT NULL,
-  `copyright` varchar(1024) DEFAULT NULL,
-  `record` varchar(1024) DEFAULT NULL,
-  `download_dir_max_size` bigint(20) NOT NULL DEFAULT '-1',
-  `download_dir_max_num` bigint(20) NOT NULL DEFAULT '-1',
-  `default_total_size_limit` bigint(20) NOT NULL DEFAULT '-1',
-  `allow_register` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`)
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `idx_mu` (`matter_uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tank30_matter` (
@@ -103,82 +99,66 @@ CREATE TABLE `tank30_matter` (
   KEY `idx_uu` (`user_uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tank30_image_cache` (
+CREATE TABLE `tank30_preference` (
   `uuid` char(36) NOT NULL DEFAULT '',
   `sort` bigint(20) NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `name` varchar(255) NOT NULL,
-  `user_uuid` char(36) DEFAULT NULL,
-  `username` varchar(45) NOT NULL,
-  `matter_uuid` char(36) DEFAULT NULL,
-  `matter_name` varchar(255) NOT NULL,
-  `mode` varchar(512) DEFAULT NULL,
-  `md5` varchar(45) DEFAULT NULL,
-  `size` bigint(20) NOT NULL DEFAULT '0',
-  `path` varchar(512) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `logo_url` varchar(255) DEFAULT NULL,
+  `favicon_url` varchar(255) DEFAULT NULL,
+  `copyright` varchar(1024) DEFAULT NULL,
+  `record` varchar(1024) DEFAULT NULL,
+  `download_dir_max_size` bigint(20) NOT NULL DEFAULT '-1',
+  `download_dir_max_num` bigint(20) NOT NULL DEFAULT '-1',
+  `default_total_size_limit` bigint(20) NOT NULL DEFAULT '-1',
+  `allow_register` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`),
-  KEY `idx_mu` (`matter_uuid`)
+  UNIQUE KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tank30_footprint` (
+CREATE TABLE `tank30_session` (
   `uuid` char(36) NOT NULL DEFAULT '',
   `sort` bigint(20) NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
   `user_uuid` char(36) DEFAULT NULL,
   `ip` varchar(128) NOT NULL,
-  `host` varchar(45) NOT NULL,
-  `uri` varchar(255) NOT NULL,
-  `params` text,
-  `cost` bigint(20) NOT NULL DEFAULT '0',
-  `success` tinyint(1) NOT NULL DEFAULT '0',
+  `expire_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
   PRIMARY KEY (`uuid`),
   UNIQUE KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tank30_download_token` (
+CREATE TABLE `tank30_share` (
+  `uuid` char(36) NOT NULL DEFAULT '',
+  `sort` bigint(20) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
+  `name` varchar(255) DEFAULT NULL,
+  `share_type` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `user_uuid` char(36) DEFAULT NULL,
+  `download_times` bigint(20) NOT NULL DEFAULT '0',
+  `code` varchar(45) NOT NULL,
+  `expire_infinity` tinyint(1) NOT NULL DEFAULT '0',
+  `expire_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tank30_upload_token` (
   `uuid` char(36) NOT NULL DEFAULT '',
   `sort` bigint(20) NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
   `user_uuid` char(36) NOT NULL,
+  `folder_uuid` char(36) NOT NULL,
   `matter_uuid` char(36) NOT NULL,
   `expire_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
+  `filename` varchar(255) NOT NULL,
+  `privacy` tinyint(1) NOT NULL DEFAULT '0',
+  `size` bigint(20) NOT NULL DEFAULT '0',
   `ip` varchar(128) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`),
-  KEY `idx_mu` (`matter_uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `tank30_dashboard` (
-  `uuid` char(36) NOT NULL DEFAULT '',
-  `sort` bigint(20) NOT NULL,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `invoke_num` bigint(20) NOT NULL,
-  `total_invoke_num` bigint(20) NOT NULL DEFAULT '0',
-  `uv` bigint(20) NOT NULL DEFAULT '0',
-  `total_uv` bigint(20) NOT NULL DEFAULT '0',
-  `matter_num` bigint(20) NOT NULL DEFAULT '0',
-  `total_matter_num` bigint(20) NOT NULL DEFAULT '0',
-  `file_size` bigint(20) NOT NULL DEFAULT '0',
-  `total_file_size` bigint(20) NOT NULL DEFAULT '0',
-  `avg_cost` bigint(20) NOT NULL DEFAULT '0',
-  `dt` varchar(45) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  UNIQUE KEY `uuid` (`uuid`),
-  KEY `idx_dt` (`dt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `tank30_bridge` (
-  `uuid` char(36) NOT NULL DEFAULT '',
-  `sort` bigint(20) NOT NULL,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `create_time` timestamp NOT NULL DEFAULT '2018-01-01 00:00:00',
-  `share_uuid` char(36) DEFAULT NULL,
-  `matter_uuid` char(36) DEFAULT NULL,
   PRIMARY KEY (`uuid`),
   UNIQUE KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
