e4799f0045debedc0176485e71dfa8af4546d244
seaheart <ycyin8795@163.com>
2023-05-27 15:28:29 +0800

    change space member create and edit key

--- code/rest/space_member_controller.go
+++ code/rest/space_member_controller.go
@@ -71,7 +71,7 @@ func (this *SpaceMemberController) Create(writer http.ResponseWriter, request *h
 
 	spaceUuid := util.ExtractRequestString(request, "spaceUuid", "spaceUuid is required")
 	userUuid := util.ExtractRequestString(request, "userUuid", "userUuid is required")
-	spaceRole := util.ExtractRequestString(request, "spaceRole", "spaceRole is required")
+	spaceRole := util.ExtractRequestString(request, "role", "spaceRole is required")
 
 	if spaceRole != SPACE_MEMBER_ROLE_READ_ONLY && spaceRole != SPACE_MEMBER_ROLE_READ_WRITE && spaceRole != SPACE_MEMBER_ROLE_ADMIN {
 		panic("spaceRole is not correct")
@@ -100,7 +100,7 @@ func (this *SpaceMemberController) Create(writer http.ResponseWriter, request *h
 
 func (this *SpaceMemberController) Edit(writer http.ResponseWriter, request *http.Request) *result.WebResult {
 	uuid := util.ExtractRequestString(request, "uuid", "uuid is required")
-	spaceRole := util.ExtractRequestString(request, "spaceRole", "spaceRole is required")
+	spaceRole := util.ExtractRequestString(request, "role", "spaceRole is required")
 
 	if spaceRole != SPACE_MEMBER_ROLE_READ_ONLY && spaceRole != SPACE_MEMBER_ROLE_READ_WRITE && spaceRole != SPACE_MEMBER_ROLE_ADMIN {
 		panic("spaceRole is not correct")
