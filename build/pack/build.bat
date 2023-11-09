c64475713589cc68ad8e3da1a4c0dc040a50fac2
lishuang <lish516@126.com>
2023-11-10 00:06:02 +0800

    feat: release 4.0.2

--- build/pack/build.bat
+++ build/pack/build.bat
@@ -11,7 +11,7 @@
 @REM prepare the variables.
 
 @REM  version name
-SET VERSION_NAME=tank-4.0.1
+SET VERSION_NAME=tank-4.0.2
 ECHO VERSION_NAME: %VERSION_NAME%
 @REM  golang proxy
 SET GOPROXY=https://goproxy.cn
