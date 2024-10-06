b4c0455f7047d303e49cd2cf98b0df33a6bf635c
yinyuchen <ycyin8795@163.com>
2024-10-06 20:51:06 +0800

    fix: space matter preview token logic

--- build/pack/build.bat
+++ build/pack/build.bat
@@ -11,7 +11,7 @@
 @REM prepare the variables.
 
 @REM  version name
-SET VERSION_NAME=tank-4.1.0
+SET VERSION_NAME=tank-4.1.1
 ECHO VERSION_NAME: %VERSION_NAME%
 @REM  golang proxy
 SET GOPROXY=https://goproxy.cn
